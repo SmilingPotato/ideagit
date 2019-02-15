bffcd47388810ec7cd9e791db924ccc5eea3a95a
syl <sysu_shaoyul@163.com>
2019-02-15 23:34:17 +0800

    修改

---
+++ gitdemo1/demo1/src/main/java/com/itheima/Customer.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+/***
+ *@ClassName:Customer
+ *@Desctiption TODO
+ *@Author:shaoyuliang
+ *@Date 2019/2/15 23:33
+ *@Version 1.0
+ ****/
+public class Customer {
+    private int age;
+}
