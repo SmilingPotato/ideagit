877803f7884a9bb936d0aff3e2b7c616e54721df
syl <sysu_shaoyul@163.com>
2019-02-15 22:55:42 +0800

    提交项目

---
+++ gitdemo1/demo1/src/main/java/com/itheima/User.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+/***
+ *@ClassName:User
+ *@Desctiption TODO
+ *@Author:shaoyuliang
+ *@Date 2019/2/15 22:53
+ *@Version 1.0
+ ****/
+public class User {
+    private int age;
+}
